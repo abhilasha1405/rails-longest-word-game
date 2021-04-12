67379e3442ddd2520033139754294fea01cc6f55
Abhilasha Dasari <abhilasha.dasari@gmail.com>
2021-04-12 15:46:39 +0200

    Longest Word Game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,44 @@
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ("A".."Z").to_a.sample }
+  end
+
+  def score
+    @attempt = params[:word]
+    @letters = params[:letters]
+    @start_time = DateTime.parse(params[:start_time])
+    @end_time = Time.now
+    @result = run_game(@attempt, @letters, @start_time, @end_time)
+  end
+
+  def run_game(attempt, grid, start_time, end_time)
+    result = { time: end_time - start_time }
+    score_and_message = score_and_message(attempt, grid, result[:time])
+    result[:score] = score_and_message.first
+    result[:message] = score_and_message.last
+    result
+  end
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json["found"]
+  end
+
+  def score_and_message(attempt, grid, time)
+    if included?(attempt.upcase, grid)
+      if english_word?(attempt)
+        score = compute_score(attempt, time)
+        [score, "Good job! #{attempt.upcase} is an English word that can be spelt from the grid."]
+      else
+        [0, "#{attempt.upcase} is not an English word."]
+      end
+    else
+      [0, "#{attempt.upcase} cannot be spelt from the letters in the grid."]
+    end
+  end
+end
